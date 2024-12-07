21d7226fc2fc9d229fcf600503175e1354b93817
Oleg Korol <oleg@olegkorol.de>
2024-12-07 15:12:24 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 
-valid_commands = ['cd']
+valid_commands = ["exit"]
 
 def main():
     while True:
@@ -8,11 +8,18 @@ def main():
         sys.stdout.flush()
 
         # Wait for user input
-        command = input()
+        command = input().split(" ")
 
-        if command not in valid_commands:
-            print(f"{command}: command not found")
+        # Check user input
+        if not command[0] or command[0] not in valid_commands:
+            print(f"{command[0]}: command not found")
             continue
+
+        if command[0] == "exit":
+            if command[1] and command[1] == 1:
+                sys.exit(1)
+            else:
+                sys.exit(0)
 
 
 if __name__ == "__main__":
